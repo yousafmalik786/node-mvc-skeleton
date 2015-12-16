984ff96712ee0e8a902e730528f626c961819736
Yousf <yousafml786@gmail.com>
2015-12-16 21:03:39 +0500

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mysql = require('mysql');
+
+var configDB = require('./config/database.js');
+
---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'url' : 'localhost',
+    'username' : 'root',
+    'pass'     : 'root',
+    'port'     :  3306 
+
+};
